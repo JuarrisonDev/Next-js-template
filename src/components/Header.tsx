df22bd95bdd762b915f25f6a7aa24a9ef38b26c1
Franco Juarez <franjuaache@gmail.com>
2025-02-18 10:46:26 -0300

    Ajustes en el header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { Transition } from '@headlessui/react';
 import { HiOutlineXMark, HiBars3 } from 'react-icons/hi2';
-import { FaFingerprint } from 'react-icons/fa';
+import Image from 'next/image'
 
 import Container from './Container';
 import { siteDetails } from '@/data/siteDetails';
@@ -23,10 +23,12 @@ const Header: React.FC = () => {
                 <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
                     {/* Logo */}
                     <Link href="/" className="flex items-center gap-2">
-                        <FaFingerprint className="text-foreground min-w-fit w-7 h-7" />
-                        <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
-                            {siteDetails.siteName}
-                        </span>
+                        <Image
+                            src="/images/logo.png"
+                            width={60}
+                            height={60}
+                            alt="The bussiness logo"
+                        />
                     </Link>
 
                     {/* Desktop Menu */}
